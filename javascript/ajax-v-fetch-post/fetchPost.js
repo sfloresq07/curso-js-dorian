3ffaf5b899a6db07bb054288e79dd393293c716f
Sandra Flores <sfloresq07@gmail.com>
2022-08-04 11:42:20 -0400

    Fetch peticiones a traves del metodo POST

---
+++ javascript/ajax-v-fetch-post/fetchPost.js
@@ -0,0 +1,34 @@
+/* 
+    Fetch API
+        Para hace peticiones POST, fetch admite un segundo parámetro.
+        fetch(url, {
+            method: 'POST',
+            body: Los datos que enviamos. Si es un objeto hay que convertirlo en JSON.stringify(datos),
+            headers: {
+                cabeceras de información sobre lo que que estamos enviando 
+                https://developer.mozilla.org/es/docs/Web/HTTP/Headers
+            }
+        })
+    console.log(newPost)
+    console.log(JSON.stringify(newPost));
+*/
+
+const button = document.getElementById('button')
+
+button.addEventListener('click', () =>{
+    const newPost ={
+        title: 'A new post',
+        body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        userId:1
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        body: JSON.stringify(newPost),
+        headers:{
+            "content-type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
